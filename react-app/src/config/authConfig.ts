8a7edb650c86d2b6e5a32b6e768005e2a733e855
lbo142962 <lbo142962@stud.gibb.ch>
2024-06-10 23:49:34 +0200

    Update authConfig.ts

--- react-app/src/config/authConfig.ts
+++ react-app/src/config/authConfig.ts
@@ -2,7 +2,7 @@ import { Configuration, PopupRequest } from '@azure/msal-browser';
 
 const msalConfig: Configuration = {
   auth: {
-    clientId: '1ea00890-3bbe-4073-946a-6cb731680f37',
+    clientId: '492e5d8e-90c6-4a70-8ced-8ebbd892859f',
     authority: 'https://login.microsoftonline.com/38a3f06c-4837-4dba-abb4-7a03b39bbf5f',
     redirectUri: 'http://localhost:3000'
   },
